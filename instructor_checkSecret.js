8151e6f3f5df4273e5934aef2758e14800f61142
Lex Olivo <aolivo@pros.com>
2023-10-31 17:32:14 -0500

    Secrets

--- instructor_checkSecret.js
+++ instructor_checkSecret.js
@@ -1,21 +1,21 @@
- function promptForPasswordAndRedirect() {
-      // Show the custom password input field
-      document.getElementById('password-input').style.display = 'block';
-    }
+function promptForSecretAndRedirect() {
+  // Show the custom secret input field
+  document.getElementById('secret-input').style.display = 'block';
+}
 
-    function submitPassword() {
-      // Get the entered password
-      const password = document.getElementById('password').value;
+function submitSecret() {
+  // Get the entered secret
+  const secret = document.getElementById('secret').value;
 
-      // Define the correct password
-      const correctPassword = 'Trainer@PROS#01';
+  // Define the correct secret
+  const correctSecret = 'Trainer@PROS#01';
 
-      // Check if the entered password matches the correct password
-      if (password === correctPassword) {
-        // Password is correct, so redirect to 'pros.com'
-        window.location.href = 'https://prostraining.github.io/class/instructor/Jeporady_for_Class_v2.html';
-      } else {
-        // Password is incorrect, display an alert
-        alert('Incorrect password. Please try again.');
-      }
-    }
+  // Check if the entered secret matches the correct secret
+  if (secret === correctSecret) {
+    // Secret is correct, so redirect to 'pros.com'
+    window.location.href = 'https://prostraining.github.io/class/instructor/Jeporady_for_Class_v2.html';
+  } else {
+    // Secret is incorrect, display an alert
+    alert('Incorrect secret. Please try again.');
+  }
+}
