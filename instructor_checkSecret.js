4ba5011d3e44d60e789b445291dc11e74f71987f
Lex Olivo <aolivo@pros.com>
2023-10-31 13:35:20 -0500

    Instructor web content

--- instructor_checkSecret.js
+++ instructor_checkSecret.js
@@ -1,8 +1,21 @@
-  function promptForPasswordAndRedirect() {
-            var password = prompt("Please enter the password:");
-            if (password === "Apple001train") { 
-                window.location.href = "https://prostraining.github.io/class/instructor/Jeporady_for_Class_v2.html";
-            } else {
-                alert("Incorrect password. Access denied.");
-            }
-        }
+ function promptForPasswordAndRedirect() {
+      // Show the custom password input field
+      document.getElementById('password-input').style.display = 'block';
+    }
+
+    function submitPassword() {
+      // Get the entered password
+      const password = document.getElementById('password').value;
+
+      // Define the correct password
+      const correctPassword = 'Apple001train';
+
+      // Check if the entered password matches the correct password
+      if (password === correctPassword) {
+        // Password is correct, so redirect to 'pros.com'
+        window.location.href = 'https://prostraining.github.io/class/instructor/Jeporady_for_Class_v2.html';
+      } else {
+        // Password is incorrect, display an alert
+        alert('Incorrect password. Please try again.');
+      }
+    }
