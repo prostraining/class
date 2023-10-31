e5b5f7772328b851b25125ae461b0cad9d2ee8a5
Lex Olivo <aolivo@pros.com>
2023-10-31 12:47:56 -0500

    Instructor changes

--- instructor_checkSecret.js
+++ instructor_checkSecret.js
@@ -1,7 +1,7 @@
   function promptForPasswordAndRedirect() {
             var password = prompt("Please enter the password:");
-            if (password === "Apple001train") { // Replace 'your_password_here' with the actual password
-                window.location.href = "https://prostraining.github.io/class/instructor/instructor_content.html";
+            if (password === "Apple001train") { 
+                window.location.href = "https://prostraining.github.io/class/instructor/Jeporady_for_Class_v2.html";
             } else {
                 alert("Incorrect password. Access denied.");
             }
